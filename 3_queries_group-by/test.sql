248e4a15cf0e89021ecb0e723fb063568fe748a6
eze1er <papaezechiel@gmail.com>
2022-02-10 05:33:09 +0000

    Set up the Javascript project

--- 3_queries_group-by/test.sql
+++ 3_queries_group-by/test.sql
@@ -1,4 +1,43 @@
-SELECT assignment_id
-FROM assignment_submissions
-JOIN students ON students.id = student_id
-WHERE students.name = 'Ibrahim Schimmel';
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(15) NOT NULL
+);
+
+CREATE TABLE property (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  title VARCHAR(255),
+  description VARCHAR(32),
+  costpernight VARCHAR(255),
+  parking_space INTEGER REFERENCES,
+  numberofbathroom INTEGER REFERENCES,
+  numberofbedroom INTEGER REFERENCES,
+  property_image IMG thumbnail_photo(id) ON DELETE CASCADE,
+  is_active  
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  github VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
+
